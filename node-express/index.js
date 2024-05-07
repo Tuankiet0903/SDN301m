28dbbec4d995a77878f7dc840439a2fd979ac47a
Kiet0903 <htk09032003@gmail.com>
2024-05-07 16:22:35 +0700

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
+const dishRouter = require('./routes/dishRouter')
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log(req.headers);
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/html");
-//   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
-// });
-
 app.use(morgan("dev"));
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
+
+
 
 app.all("/dishes", (req, res, next) => {
   res.statusCode = 200;
